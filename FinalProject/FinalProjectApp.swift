0f24c2ffa966843eb4f12a3dd49efc4c637b8954
Christiana Tweedt <Tweedt@bc.edu>
2022-11-29 18:41:56 -0500

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Christiana Tweedt  on 11/29/22.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
